9678ed85dae1e5df2e1a4244d8dd1d335caec311
IvanLabza <100775210+IvanLabza@users.noreply.github.com>
2026-07-23 14:21:01 +0300

    task 2 finish

--- js/variables.js
+++ js/variables.js
@@ -11,7 +11,7 @@ const lastName = "Labza";
 console.log(lastName);
 
 // let, тому що вік з часом змінюється
-let age = 30;
+let age = 22;
 console.log(age);
 
 // let, тому що місце проживання може змінитися
@@ -29,3 +29,17 @@ console.log(birthYear);
 // const, тому що улюблений колір задається як постійне значення
 const favoriteColor = "black";
 console.log(favoriteColor);
+
+// 2 Task
+
+// вік через рік
+
+console.log(age + 1);
+
+// Uncaught TypeError: Assignment to constant variable. at variables.js:37:24
+
+// birthYear = 2000;
+
+// Uncaught SyntaxError: Identifier 'firstName' has already been declared (at variables.js:43:7)
+
+// const firstName = "Vitalii";
