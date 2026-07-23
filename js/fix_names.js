74283147973e54a220bfceed36208c96e72cef72
IvanLabza <100775210+IvanLabza@users.noreply.github.com>
2026-07-23 16:22:57 +0300

    fix names

---
+++ js/fix_names.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let age = 25;
+let name = "Anna";
+let userName = "Bob";
+let userAge = 30;
+let userFirstName = "Charlie";
+let userLastName = "Smith";
+let numberOfUsers = 100000;
+let userData = { age: 20 };
