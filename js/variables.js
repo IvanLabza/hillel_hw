b4b219b95d8e81c0d141019064a4344acc6391a1
IvanLabza <100775210+IvanLabza@users.noreply.github.com>
2026-07-23 13:01:51 +0300

    1 task finish

--- js/variables.js
+++ js/variables.js
@@ -1,1 +1,31 @@
 "use strict";
+
+// 1 Task
+
+// const, тому що ім'я не буде змінюватися
+const firstName = "Ivan";
+console.log(firstName);
+
+// const, тому що прізвище є постійним
+const lastName = "Labza";
+console.log(lastName);
+
+// let, тому що вік з часом змінюється
+let age = 30;
+console.log(age);
+
+// let, тому що місце проживання може змінитися
+let city = "Poltava";
+console.log(city);
+
+// const, тому що хобі задається як незмінне значення
+const hobby = "gaming";
+console.log(hobby);
+
+// const, тому що рік народження ніколи не змінюється
+const birthYear = 2004;
+console.log(birthYear);
+
+// const, тому що улюблений колір задається як постійне значення
+const favoriteColor = "black";
+console.log(favoriteColor);
