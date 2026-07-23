d00b5c5fe2d82264c86cfa0f0bfc6b2aeca94137
IvanLabza <100775210+IvanLabza@users.noreply.github.com>
2026-07-23 16:28:14 +0300

    add config js

---
+++ js/config.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const API_BASE_URL = "https://api.example.com";
+
+const MAX_RETRIES = 3;
+
+const WHITE_COLOR = "#FFFFFF";
+
+const DB_USERS = "localhost";
+
+const DB_PASSWORD = "password123";
