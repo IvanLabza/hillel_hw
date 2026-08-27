55d9b46c8f6f0eb83040fd97f4721a2b7d3ef816
IvanLabza <100775210+IvanLabza@users.noreply.github.com>
2026-08-27 14:45:50 +0300

    finish task

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ##Home Work Hillel
+
+ Реалізуйте функцію generateKey
--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+function generateKey(length, characters) {
+  let key = "";
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    key += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return key;
+}
+
+const key = generateKey(3, characters);
+console.log(key); 
