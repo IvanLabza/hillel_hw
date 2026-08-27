723c0a3120a612ba16767e1df49cb27bd8371a84
IvanLabza <100775210+IvanLabza@users.noreply.github.com>
2026-08-27 15:25:40 +0300

    finish task

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ##Home Work Hillel
+
+Реалізував функцію removeElement
--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+const removeElement = (arr, element) => {
+  const index = arr.indexOf(element);
+  if (index !== -1) {
+    arr.splice(index, 1);
+  }
+};
+
+removeElement(array, 3);
+console.log(array);
+// Результат: [1, 2, 4, 5, 6, 7]
