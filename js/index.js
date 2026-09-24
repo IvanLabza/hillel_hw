17a0a486ec5ff575c35e5a2f62a739296361793b
IvanLabza <100775210+IvanLabza@users.noreply.github.com>
2026-09-24 15:14:05 +0300

    finish task

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const input = document.querySelector("#input");
+const message = document.querySelector("#message");
+
+input.addEventListener("focus", () => {
+  message.hidden = false;
+});
+
+input.addEventListener("blur", () => {
+  message.hidden = true;
+});
