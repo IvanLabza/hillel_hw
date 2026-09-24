c19f315f19fdbc34df17f203484822d412a769fb
IvanLabza <100775210+IvanLabza@users.noreply.github.com>
2026-09-24 14:50:16 +0300

    finish task

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ##Home Work Hillel
+
+ДЗ 22. Написати свою реалізацію функції bind, call, aplly
--- js/index.js
+++ js/index.js
@@ -1,1 +1,47 @@
 "use strict";
+
+// CALL
+
+const myCall = (fn, obj, ...args) => {
+  obj.temp = fn;
+  const result = obj.temp(...args);
+  delete obj.temp;
+
+  return result;
+};
+
+// APPLY
+
+const myApply = (fn, obj, args) => {
+  obj.temp = fn;
+  const result = obj.temp(...args);
+  delete obj.temp;
+
+  return result;
+};
+
+// BIND
+
+const myBind = (fn, obj, ...args) => {
+  return (...newArgs) => {
+    return myApply(fn, obj, [...args, ...newArgs]);
+  };
+};
+
+// ПРИКЛАД
+
+const greet = function (age, city) {
+  return `${this.name}, ${age}, ${city}`;
+};
+
+const user = {
+  name: "Ivan",
+};
+
+console.log(myCall(greet, user, 25, "Poltava"));
+
+console.log(myApply(greet, user, [25, "Poltava"]));
+
+const newGreet = myBind(greet, user, 25);
+
+console.log(newGreet("Poltava"));
